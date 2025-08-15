e2e59aa51cdc9476ee75ab6db1312dd4af8041e5
IverMartinson <iver.m.martinson@gmail.com>
2025-08-15 18:28:27 -0400

    moved all headers into sourparse.c

--- src/headers/sourparse.h
+++ src/headers/sourparse.h
@@ -1,8 +1,22 @@
 #ifndef SOURPARSE_H
 #define SOURPARSE_H
 
-#include "functions.h"
-#include "values.h"
-#include "custom_types.h"
+#include "stdint.h"
+
+typedef struct {
+    int *x_coords, *y_coords, *contour_end_indicies, number_of_points, number_of_contours;
+    uint8_t *flags;
+} SP_glyph;
+
+typedef struct {
+    int current_byte, number_of_glyphs, *glyph_offsets; 
+    uint16_t *unicode_to_glyph_indicies, units_per_em;
+    uint8_t *buffer;
+    SP_glyph *glyphs;
+    int16_t index_to_loca_format;
+} SP_font;
+
+SP_font SP_load_font(char *filename);
+void SP_free_font(SP_font* font);
 
 #endif
